b66fe56d8a27453db495e3622c575a0915f0debf
Erik Polderman <eriksnoww@gmail.com>
2017-08-13 16:19:20 -0500

    google strat settings

--- services/passport.js
+++ services/passport.js
@@ -13,7 +13,8 @@ passport.use(new GoogleStrategy(
   //configuration options
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback'
+  callbackURL: '/auth/google/callback',
+  proxy: true
   },
   (accessToken, refreshToken, profile, done) => {
 

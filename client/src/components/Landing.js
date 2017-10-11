4b59ae8c5fe60498d1e31eec42cecc12df02a915
Erik Polderman <eriksnoww@gmail.com>
2017-10-10 22:43:58 -0500

    fixed flip animation part 6900

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -14,10 +14,10 @@ const Landing = () => {
         <div className="landingMain">
             <div className="landingWrapper robot">
                 <Link className="landingWrapper-item medium adidas" to={'/boots/Adidas'}>
-                    <div className="front face">
+                    <div className="front">
                       <img src={require('../Assets/adidas.svg')} alt={"Adidas"}/>
                     </div>
-                    <div className="back face center" style={{backgroundColor: '#8A2BE2'}}>
+                    <div className="back center" style={{backgroundColor: '#8A2BE2'}}>
                       <div className="envelope">
                         <span className="fa fa-envelope-open fa-5x" aria-hidden="true"></span>
                         <p>Review</p>

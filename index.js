66a1e7900e9b8e6d986fcfda9985dded183c7b20
Erik Polderman <eriksnoww@gmail.com>
2018-12-08 17:56:37 -0700

    fix: mongo connect deprecation warning

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 
+const options = {
+  useMongoClient: true
+};
 
 //(function)run mongoose to create schema, models, etc..
 require('./models/User');
@@ -15,19 +18,22 @@ require('./models/Boot');
 require('./services/passport');
 //set up config that will listen to requests from nodeJS
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURL);
+mongoose.connect(
+  keys.mongoURL,
+  options
+);
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //middleware (handle something) before sent to route handlers
 //cookieSession processes req and populates req.id and passes over to passport
 //then to route handlers () ->
 //limited to 4000bytes or 4K KB
 app.use(
-  cookieSession ({
-      maxAge: 30 * 24 * 60 * 60 * 1000,
-      keys: [keys.cookieKey]
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
   })
 );
 app.use(passport.initialize());
@@ -44,7 +50,7 @@ require('./routes/bootRoutes')(app);
 //there are some routes express is ready for
 //there are even more routes that express should say hey here is my index.html
 //there are more routes that want to access specific files like index.js
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
   //production code | express serves up assets
   //main.js or main.css file,etc.. -> look into our client/build dir
   app.use(express.static('client/build'));
@@ -53,7 +59,7 @@ if(process.env.NODE_ENV === 'production'){
   //kick user to client side of our app(React Router handles the rest)
   //must catch anything that falls through
   const path = require('path');
-  app.get('*', (req,res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
